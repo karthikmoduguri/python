70d5232f0b5a217d795e735c2518338f6d91b075
MODUGURI KARTHIK <karthikmoduguri@gmail.com>
2025-07-02 10:51:29 +0000

    dict questions

---
+++ classquestions/dict.py
@@ -0,0 +1,22 @@
+#merge two dictionaries
+# d1={"one":1,"two":2}
+# d2={"three":3,"four":4}
+# print(d1|d2)
+
+
+# merged={**d1,**d2} or d1.update(d2)
+
+#unique values of dictionary
+# d1={"one":1,"two":2}
+# d2={"three":3,"four":1}
+# print(set(d1.values())^set(d2.values()))
+#same for keys also
+
+
+# s=input().split()
+# print(max(s,key=len))
+
+d1={"one":1,"two":2}
+print(d1.copy())
+# l=[5,6]
+# print(d1.update(l))
